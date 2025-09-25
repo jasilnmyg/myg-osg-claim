9a14599268a9fc58c30756a5172cef1643fe0547
jasilnmyg <mygloyalty3@gmail.com>
2025-09-25 16:04:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from email.mime.application import MIMEApplication
 # ----------------------
 # CONFIG
 # ----------------------
-EXCEL_FILE = "/workspaces/myg-osg-claim/OSID DATA.xlsx"
+EXCEL_FILE = "OSID DATA.xlsx"
 TARGET_EMAIL = "akhilmp@myg.in"
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
