df0af8ae9db10e7e943c4b71b23d005132bf848b
jasilnmyg <mygloyalty3@gmail.com>
2025-10-16 12:07:17 +0530

    Add new email to CC_EMAILS list

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,7 @@ st.markdown("""
 # ----------------------
 EXCEL_FILE = "OSID DATA.xlsx"
 TARGET_EMAIL = "shyla.mariadhasan@onsite.co.in"
-CC_EMAILS = ["shine.at@onsite.co.in", "akhilmp@myg.in","sachin.kadam@onsite.co.in","shanmugaraja.a@onsite.co.in"]
+CC_EMAILS = ["shine.at@onsite.co.in", "akhilmp@myg.in","sachin.kadam@onsite.co.in","shanmugaraja.a@onsite.co.in","akhil.chandran@onsite.co.in"]
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 SENDER_EMAIL = "jasil@myg.in"
