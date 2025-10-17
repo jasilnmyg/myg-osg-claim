13cdd321f91864f8896a976fb81fa0310f6430dd
jasilnmyg <mygloyalty3@gmail.com>
2025-10-17 17:54:22 +0530

    Update email and file path configurations

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,9 +152,9 @@ st.markdown("""
 # ----------------------
 # CONFIG
 # ----------------------
-EXCEL_FILE = "OSID DATA.xlsx"
-TARGET_EMAIL = "mygloyalty3@gmail.com"
-CC_EMAILS = ["shahin@myg.in"]
+EXCEL_FILE = "/workspaces/myg-osg-claim/OSID DATA.xlsx"
+TARGET_EMAIL = "shyla.mariadhasan@onsite.co.in"
+CC_EMAILS = ["shine.at@onsite.co.in", "akhilmp@myg.in","sachin.kadam@onsite.co.in","shanmugaraja.a@onsite.co.in","akhil.chandran@onsite.co.in"]
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 SENDER_EMAIL = "jasil@myg.in"
