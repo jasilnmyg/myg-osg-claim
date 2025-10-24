37fae493cf7eea420cabff5ee4d3d5181052afee
jasilnmyg <mygloyalty3@gmail.com>
2025-10-24 11:35:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,8 +128,8 @@ st.markdown("""
 # CONFIG
 # ----------------------
 EXCEL_FILE = "OSID DATA.xlsx"
-TARGET_EMAIL = "mygloyalty3@gmail.com"
-CC_EMAILS = ["shahin@myg.in"]
+TARGET_EMAIL = "shyla.mariadhasan@onsite.co.in"
+CC_EMAILS = ["shine.at@onsite.co.in", "akhilmp@myg.in","sachin.kadam@onsite.co.in","shanmugaraja.a@onsite.co.in","akhil.chandran@onsite.co.in"]
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 SENDER_EMAIL = "jasil@myg.in"
