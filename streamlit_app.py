2dd5be1aa948c9cd3e7eae4e13e783b6a921e4ed
jasilnmyg <mygloyalty3@gmail.com>
2025-10-31 12:14:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,7 +127,7 @@ st.markdown("""
 # ----------------------
 # CONFIG
 # ----------------------
-EXCEL_FILE = "/workspaces/myg-osg-claim/Onsitego OSID (1).xlsx"
+EXCEL_FILE = "Onsitego OSID (1).xlsx"
 TARGET_EMAIL = "shyla.mariadhasan@onsite.co.in"
 CC_EMAILS = ["shine.at@onsite.co.in", "akhilmp@myg.in","sachin.kadam@onsite.co.in","shanmugaraja.a@onsite.co.in","akhil.chandran@onsite.co.in"]
 SMTP_SERVER = "smtp.gmail.com"
